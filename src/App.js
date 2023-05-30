06a92903168d5059df315f2fb2aa0c9a87c89bbb
Jay <sinaelj@gmail.com>
2023-05-30 22:46:06 +0900

    ♻️ 마운트할 때 useEffect가 실행되도록 Route 경로 수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
-import React, { useState } from 'react';
+import React, { useState, } from 'react';
 import DisplayWeather from './Component/DisplayWeather';
 import Search from './Component/Search';
 import styled from 'styled-components';
@@ -8,10 +8,14 @@ import home from './img/home.svg'
 import search from './img/search.svg'
 
 export default function App() {
+  // 검색창에 입력한 값을 state로 관리
   const [inputLocation, setInputLocation] = useState('');
   const [preInput, setPreInput] = useState('');
 
+  // 현재 시간을 받아와서 props로 전달
   const currentTime = new Intl.DateTimeFormat('en', { hour: '2-digit', hourCycle: 'h23' }).format(new Date())
+
+
 
   return (
     <>
@@ -19,7 +23,7 @@ export default function App() {
       <h1 className='a11y-hidden'>WEATHER REPORT</h1>
       <BrowserRouter>
         <Header>
-          <Nav icon="home" to="/">
+          <Nav icon="home" to="/weatherSPA">
             <span className="a11y-hidden">홈</span>
           </Nav>
           <Nav icon="search" to="/search/">
@@ -29,9 +33,8 @@ export default function App() {
 
         <h2 className="a11y-hidden">날씨 정보</h2>
         <Routes>
-          <Route path="" element={<DisplayWeather inputLocation={inputLocation} />} />
+          <Route path="/weatherSPA" element={<DisplayWeather inputLocation={inputLocation} />} />
           <Route path="/search" element={<Search setInputLocation={setInputLocation} preInput={preInput} setPreInput={setPreInput} />} />
-
         </Routes>
       </BrowserRouter>
     </>
