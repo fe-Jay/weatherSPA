ec9232c80f07bd8f2f83c3d6e2d76931a3c35053
Jay <sinaelj@gmail.com>
2023-05-30 17:52:11 +0900

    💅 Spinner 애니메이션 적용

---
+++ src/Component/Loading.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import styled from "styled-components";
+
+export const Loading = () => {
+    return (
+        <Spinner>
+            <span className="a11y-hidden">Loading...</span>
+        </Spinner>
+    )
+}
+
+const Spinner = styled.div`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 50px;
+    height: 50px;
+    margin: -25px 0 0 -25px;
+    border-radius: 50%;
+    border: 4px solid #ffffff30;
+    border-top-color: #fff;
+    animation: spin 1s ease-in-out infinite;
+    @keyframes spin {
+        to {
+            transform: rotate(360deg);
+        }
+    }
+`
