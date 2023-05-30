8090a5c6e414157bf57f8244cf888ff4fe3b0e12
Jay <sinaelj@gmail.com>
2023-05-30 17:52:38 +0900

    💅 Styled Component 적용

--- src/Component/Search.jsx
+++ src/Component/Search.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
-
+import styled from 'styled-components';
 
 export default function Search({ setInputLocation, preInput, setPreInput }) {
     const navigate = useNavigate();
@@ -13,7 +13,8 @@ export default function Search({ setInputLocation, preInput, setPreInput }) {
     }
 
     return (
-        <article className='search'>
+        <SearchForm>
+            <h2>Local search</h2>
             <form onSubmit={submitForm}>
                 <input
                     type="text"
@@ -23,7 +24,33 @@ export default function Search({ setInputLocation, preInput, setPreInput }) {
                 />
                 <button type="submit">🔍</button>
             </form>
-        </article>
+        </SearchForm>
     )
 }
 
+const SearchForm = styled.article`
+        h2 {
+            font-size: clamp(1.5rem, 4vw, 2rem);
+            margin-bottom: 1rem;
+            text-align: left;
+        }
+        form {
+            display: flex;
+            input {
+                flex: 1 0 0;
+                padding: 0.5rem;
+                border: none;
+                border-radius: 0.5rem 0 0 0.5rem;
+                outline: none;
+                font-size: clamp(1rem, 3vw, 2vw);
+            }
+            button {
+                padding: 0.5rem;
+                border: none;
+                border-radius: 0 0.5rem 0.5rem 0;
+                outline: none;
+                font-size: clamp(1rem, 3vw, 2vw);
+                cursor: pointer;
+            }
+        }
+`
